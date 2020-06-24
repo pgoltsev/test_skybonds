b502175bf7a809c1c3a941014b847c4fd239bd4e
Pavel Goltsev <p.goltsev@gmail.com>
2020-06-24 18:05:10 +0300

    Replace input for flexibility and convenient testing

--- src/test_skybonds/fraction_percent_calculation.py
+++ src/test_skybonds/fraction_percent_calculation.py
@@ -1,24 +1,37 @@
 """The program converts fractions to their percentage values."""
 import sys
 from decimal import Decimal, DecimalException, ROUND_HALF_EVEN
+from typing import Callable, Iterable, Optional
 
 
-def read_fraction_amount() -> int:
-    input_msg = 'Input fractions amount: '
-    value = input(input_msg)
+def read_fraction_amount(read_input: Callable[[str], str]) -> int:
+    """Read fraction amount from given read stream function.
+
+    :param read_input: Function for reading input stream.
+    :return: Fraction amount.
+    """
+    input_msg: str = 'Input fractions amount: '
+    value: str = read_input(input_msg)
     while not value.isdigit():
         print_error('Incorrect input. Should be an integer.')
-        value = input(input_msg)
+        value = read_input(input_msg)
 
     return int(value)
 
 
-def read_fraction(number: int, amount: int) -> Decimal:
-    input_msg = f'Input fraction ({number} of {amount}): '
-    value = None
+def read_fraction(number: int, amount: int, read_input: Callable[[str], str]) -> Decimal:
+    """Read fraction value from given read stream function.
+
+    :param number: Number of input.
+    :param amount: Amount of fractions should be consumed.
+    :param read_input: Function for reading input stream.
+    :return: Fraction value.
+    """
+    input_msg: str = f'Input fraction ({number} of {amount}): '
+    value: Optional[Decimal] = None
     while value is None:
         try:
-            value = Decimal(input(input_msg))
+            value = Decimal(read_input(input_msg))
         except DecimalException:
             print_error('Incorrect input. Should be a rational number.')
 
@@ -26,26 +39,37 @@ def read_fraction(number: int, amount: int) -> Decimal:
 
 
 def print_error(message: str) -> None:
+    """Output the error message to standard error stream.
+
+    :param message: Message.
+    """
     sys.stderr.write(f'{message}\n')
 
 
-def main():
-    print(__doc__)
-    print('To exit just press Ctrl+C')
+def calculate_fraction_percents(read_input: Callable[[str], str]) -> Iterable[Decimal]:
+    """Calculate faction percents.
 
-    fractions_amount = read_fraction_amount()
-    fractions = [
-        read_fraction(number, fractions_amount)
+    :param read_input: Function that accepts a message which will be printed to a user, then reads input from the user
+    and returns it.
+    :return: Iterable with calculated percents.
+    """
+    print(__doc__)
+    print('To exit just press Ctrl+C.')
+
+    fractions_amount: int = read_fraction_amount(read_input)
+    fractions: list = [
+        read_fraction(number, fractions_amount, read_input)
         for number in range(1, fractions_amount + 1)
     ]
-    fractions_sum = sum(fractions)
-    quantize = Decimal('1.000')
+    fractions_sum: Decimal = sum(fractions)
+    quantize: Decimal = Decimal('1.000')
     for fraction in fractions:
-        print((fraction / fractions_sum).quantize(quantize, rounding=ROUND_HALF_EVEN))
+        yield (fraction / fractions_sum).quantize(quantize, rounding=ROUND_HALF_EVEN)
 
 
 if __name__ == '__main__':
     try:
-        main()
+        for percent in calculate_fraction_percents(input):
+            print(percent)
     except KeyboardInterrupt:
         exit(0)
